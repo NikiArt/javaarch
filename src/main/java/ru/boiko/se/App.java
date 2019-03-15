cdde3c92645134e783574320271dc49e36a9bb12
Nikita Boiko <genertyniki@gmail.com>
2019-03-15 11:48:18 +0300

    add exponentiation

--- src/main/java/ru/boiko/se/App.java
+++ src/main/java/ru/boiko/se/App.java
@@ -1,7 +1,21 @@
 package ru.boiko.se;
 
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(exp(5, 4));
     }
+
+    private static double exp(int value, int power) {
+        if (value == 0) return 0;
+        if (power == 0) return 1;
+
+        if (power > 0) {
+            return exp(value, --power) * value;
+        } else {
+            return 1 / exp(value, (power * (-1)));
+        }
+    }
+
+
 }
