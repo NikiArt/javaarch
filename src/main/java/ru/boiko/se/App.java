f86edebafd6abdb5b4b81e522ebcf9bee1471831
Nikita Boiko <genertyniki@gmail.com>
2019-03-15 11:56:36 +0300

    add exponentiation with using loop

--- src/main/java/ru/boiko/se/App.java
+++ src/main/java/ru/boiko/se/App.java
@@ -4,6 +4,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(exp(5, 4));
+        System.out.println(expLoop(2, 10));
     }
 
     private static double exp(int value, int power) {
@@ -15,6 +16,21 @@ public class App {
         } else {
             return 1 / exp(value, (power * (-1)));
         }
+    }
+
+    public static double expLoop(double value, int power) {
+        if (value == 0) return 0;
+        if (power == 0) return 1;
+
+        int currentpower = power < 0 ? power * (-1) : power;
+
+        double result = 1;
+        while (currentpower > 0) {
+            result = result * value;
+            currentpower--;
+        }
+        if (power < 0) result = 1 / result;
+        return result;
     }
 
 
