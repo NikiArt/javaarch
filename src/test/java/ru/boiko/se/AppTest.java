b395eba9c7b5fef6dbe3767933c9f2550738db35
Nikita Boiko <genertyniki@gmail.com>
2019-03-22 12:58:38 +0300

    add graph

--- src/test/java/ru/boiko/se/AppTest.java
+++ src/test/java/ru/boiko/se/AppTest.java
@@ -105,4 +105,27 @@ public class AppTest {
 
         System.out.println("Balanced trees - " + balancedP + "%");
     }
+
+    @Test
+    public void graph() {
+        Graph g = new Graph(6);
+        g.addEdge(0, 2);
+        g.addEdge(0, 1);
+        g.addEdge(1, 2);
+        g.addEdge(5, 3);
+        g.addEdge(3, 4);
+        g.addEdge(3, 2);
+        g.addEdge(4, 2);
+        g.addEdge(5, 0);
+
+        WidthFirstPaths bfp = new WidthFirstPaths(g, 0);
+        System.out.println(bfp.hasPathTo(5));
+        System.out.println(bfp.pathTo(3));
+        System.out.println(bfp.pathTo(5));
+        System.out.println(bfp.distTo(1));
+        System.out.println(bfp.distTo(2));
+        System.out.println(bfp.distTo(3));
+        System.out.println(bfp.distTo(4));
+        System.out.println(bfp.distTo(5));
+    }
 }
